621bf39e7a1bbbc661a65cceafc0b090bcfbeea8
rumman3 <rumman_123@hotmail.co.uk>
2022-03-14 20:14:34 +0000

    improved OOP EMS with exceptions & str

--- object_oriented_employee.py
+++ object_oriented_employee.py
@@ -1,8 +1,5 @@
 class Employee:
 
-    employees = {}                   ##put dictionary inside class, so it's easier to access all the details.
-                                     ##if it was outside, you can't access without 'returns'
-
     def __init__(self):
         self.firstname = ""
         self.lastname = ""
@@ -13,15 +10,18 @@ class Employee:
         self.graduate = None
         self.id = None
 
-    def read_id(self):
+    def read_id(self, employees):
         while True:
-            self.id = input("Please Enter Employee ID: ")
-            self.id = self.id.strip()
-            if (self.id.isdigit() > 0) and (self.id not in Employee.employees.keys()):
-                self.id = int(self.id)
-                break
+            try:
+                self.id = int(input("Please Enter Employee ID: "))
+            except ValueError:
+                print("The ID should only contain digits")
+                continue   ##continue brings you back to the top of the loop to try again
+            if self.id not in employees.keys():
+                return self.id
             else:
-                print("Enter another valid Employee ID:")
+                print("This ID is already taken")
+
 
     def read_first_name(self):
         while True:
@@ -99,17 +99,16 @@ class Employee:
             else:
                 print("Please Type 'y' or 'n'")
 
+    def __str__(self):
+        text = f"Employee ID: {self.id} \nEmployee First Name: {self.firstname.capitalize().strip()} " \
+               f"\nEmployee Last Name: {self.lastname.capitalize().strip()}\nEmployee Birth Date: {self.birthday}/" \
+               f"{self.birthmonth}/{self.birthyear} \nEmployee Position: {self.position.capitalize().strip()}" \
+               f"\nEmployee is a Graduate: {self.graduate}"
 
-    def print_all_data(self):
-        print(f"Employee ID: {self.id}")
-        print(f"Employee First Name: {self.firstname.capitalize().strip()}")
-        print(f"Employee Last Name: {self.lastname.capitalize().strip()}")
-        print(f"Employee Birth Date: {self.birthday}/{self.birthmonth}/{self.birthyear}")
-        print(f"Employee Position: {self.position.capitalize().strip()}")
-        print(f"Employee is a Graduate: {self.graduate}")
+        return text
 
-    def add_employee(self):
-        self.read_id()
+    def add_employee(self, employees):
+        self.read_id(employees)
         self.read_first_name()
         self.read_last_name()
         self.read_birth_day()
@@ -118,19 +117,8 @@ class Employee:
         self.read_position()
         self.read_graduate()
 
-    @staticmethod       ##Static when you make a method that isn't involving the class (i.e. not going to use Self)
-    def read_options():
-        valid_options = ["a", "r", "t", "l", "ret", "u", "e"]
-        while True:
-            options_str = input("Please select an option: \n'a' to Add Employee \n'r' to Remove Employee"
-                                "\n't' to Check Total No. of Employees \n'l' to see a list of the Employee(s)' details"
-                                "\n'ret' to Retrieve an Employee's Data \n'u' to Update an Employee Data \n'e' to Exit: ")
-            options_str = options_str.strip()
-            if options_str in valid_options:
-                return options_str
-            else:
-                print("Please Enter a Valid Option:  a | r | t | l | ret | u | e  ")
 
+    ##Static when you make a method that isn't involving the class (i.e. not going to use Self)
     @staticmethod
     def remove_employee():
         while True:
@@ -159,43 +147,67 @@ class Employee:
                 update_details = int(update_details_str)
                 return update_details
 
+def read_options(): ##outside of class (not needed in class)
+    valid_options = ["a", "r", "t", "l", "ret", "u", "e"]
+    while True:
+        options_str = input("Please select an option: \n'a' to Add Employee \n'r' to Remove Employee"
+                            "\n't' to Check Total No. of Employees \n'l' to see a list of the Employee(s)' details"
+                            "\n'ret' to Retrieve an Employee's Data \n'u' to Update an Employee Data \n'e' to Exit: ")
+        options_str = options_str.strip()
+        if options_str in valid_options:
+            return options_str
+        else:
+            print("Please Enter a Valid Option:  a | r | t | l | ret | u | e  ")
 
 if __name__ == "__main__":
 
+    employees = {}
 
     while True:
-        options = Employee.read_options()     ##access the 'read_options' static method
+        options = read_options()     ##access the 'read_options' static method
 
         if options.lower() == "a":
             employee = Employee()
-            employee.add_employee()
-            Employee.employees[employee.id] = employee ##Employee. is to access the class    ##.employees is to access dictionary
+            employee.add_employee(employees)
+            employees[employee.id] = employee ##Employee. is to access the class    ##.employees is to access dictionary
                                                        ##employee.id is to be able to access details via id
                                                        ##=employee is the variable that stores this particular employee's details
-            employee.print_all_data()
+            print(str(employee))          ## str returns all the __str__ data & print() prints it
 
         elif options.lower() == "r":
-            remove_id = Employee.remove_employee()
-            del Employee.employees[remove_id]
+            while True:
+                remove_id = Employee.remove_employee()
+                try:
+                    del employees[remove_id]
+                    break
+                except KeyError:               ## KeyError is trying to look for a key in a dictionary that does not exist
+                    print("This id does not exist")
+
             print(f"Employee with id {remove_id} is removed")
-            print(Employee.employees)
+
 
         elif options.lower() == "t":
-            total_employees = len(Employee.employees.keys()) ##returns a 'list' of all the keys (IDs) & len counts the no. of keys of dictionary
+            total_employees = len(employees.keys()) ##returns a 'list' of all the keys (IDs) & len counts the no. of keys of dictionary
             print(f"The Total No. of Employees in the system are {total_employees}")
 
         elif options.lower() == "l":
-            list_of_employees = list(Employee.employees.values()) ##values are all the employees' details
+            list_of_employees = list(employees.values()) ##values are all the employees' details
             for employee in list_of_employees:
-                employee.print_all_data()
+                print(str(employee))
 
         elif options.lower() == "ret":
-            retrieveID = Employee.retrieve_details()
-            Employee.employees[retrieveID].print_all_data()
+            while True:
+                retrieveID = Employee.retrieve_details()
+                try:
+                    print(str(employees[retrieveID]))
+                    break
+                except KeyError:
+                    print("This id does not exist")
+
 
         elif options.lower() == "u":
             change_details = Employee.update_details()   ##variable to access ID
-            employee = Employee.employees[change_details]
+            employee = employees[change_details]
             valid_inputs = ["firstname", "lastname", "birthday", "birthmonth", "birthyear", "position", "graduate"]
             while True:
                 update = input("What details would you like to update? Choose from following: "
@@ -220,7 +232,7 @@ if __name__ == "__main__":
                 employee.read_position = mod
             elif update == "graduate":
                 employee.read_graduate = mod
-            employee.print_all_data()
+            print(str(employee))
 
         else:
             break
